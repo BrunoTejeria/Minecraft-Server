c0eeb787f03ee351a210aeda1a0e9647b608859b
BrunoTejeria <tejeriabruno@gmail.com>
2023-12-31 15:34:57 -0300

    Frontend

--- Minecraft-Server-Remote/src/components/App.jsx
+++ Minecraft-Server-Remote/src/components/App.jsx
@@ -1,12 +1,43 @@
 // eslint-disable-next-line no-unused-vars
+
+
+//Importar constantes
+
+import { API_HTTP_IP, API_SOCKET_IP } from '../js/consts';
+
+
 import React from 'react';
 import axios from 'axios';
 import '../styles/App.css';
 
+
+
+
 function App() {
+  const socket = new WebSocket(`${API_SOCKET_IP}`);
+  socket.onopen = (event) => {
+    console.log("Conexión WebSocket abierta:", event);
+  };
+
+  socket.onmessage = (event) => {
+    console.log("Mensaje recibido:", event.data);
+  };
+
+  socket.onclose = (event) => {
+    console.log("Conexión WebSocket cerrada:", event);
+  };
+
+  socket.onerror = (error) => {
+    console.error("Error en la conexión WebSocket:", error);
+  };
+
+
+
+
+
   const startServer = async () => {
     try {
-      const response = await axios.post('/api/start_server');
+      const response = await axios.get(`${API_HTTP_IP}/start-server`);
       console.log(response.data);
     } catch (error) {
       console.error('Error al iniciar el servidor:', error);
---
+++ Minecraft-Server-Remote/src/js/consts.js
@@ -0,0 +1,2 @@
+export const API_HTTP_IP = "http://localhost:64"
+export const API_SOCKET_IP = "ws://localhost:512"
